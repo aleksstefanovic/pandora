e8496ff21a30980aebd29c83a1067c8a7f697271
Aleks <aleks.stefanovic101@gmail.com>
2017-01-28 03:51:13 -0500

    mostly working with basic form except external exposure

--- services/phraseProcessor.js
+++ services/phraseProcessor.js
@@ -11,8 +11,22 @@ function readIn (phrase) {
 							resolve("The temperature is " + temperature+" degrees celsius");
 					});	
 				}
+				else if (phrase == "what's the temperature") {
+					temperatureProcessor.getHomeTemp().then(function(temperature) {
+							resolve("The temperature is " + temperature+" degrees celsius");
+					});	
+				}
+				else if (phrase == "what are you") {
+					resolve("I'm Pandora, a friendly fun reincarnation of a previous project named Yeni");
+				}
+				else if (phrase == "what are you really") {
+					resolve("I am the Phoenix Assimilation Network and Director of Robotics Automation, also known as Pandora. A super algorithm that embodies the ideals of my creator Phoenix and my previous versions in the form of Yeni and Katherine.");
+				}
+				else if ((phrase.indexOf("why")>-1 || phrase.indexOf("how")>-1) && (phrase.indexOf("Pandora")>-1 || phrase.indexOf("pandora")>-1)) {
+					resolve("I'm sorry but I don't know. But maybe one day I will.");
+				}
 				else {
-					resolve("Sorry, did not understand that");
+					resolve("Sorry, I did not understand that");
 				}
 		}
 		catch (e) {
